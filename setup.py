6042b48a0fd6c5906c5a4b12a65bb4b1b2f045dc
javerianadeem <javerianadeem03@gmail.com>
2021-09-07 12:24:28 +0500

    adding dist files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='selenium_metamask_automation',
       keywords="python metamask blockchain automate-metamask install-metamask-extention-selenium selenium-metamask",
       install_requires=["selenium>=3.141.0", "pywin32"],
       python_requires='>=3.8',
-      url='https://github.com/javerianadeem/selenium-metamask-automation',
+      url='git@github.com:javerianadeem/selenium_metamask_automation',
       author='Javeria Nadeem',
       author_email='javerianadeem03@gmail.com'
 
